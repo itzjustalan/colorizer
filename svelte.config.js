3835b06e36479558923f7d6ef7683fb3e0d704cb
Alan K John <itzjustalan@gmail.com>
2022-06-22 13:22:57 +0530

    gh-pages path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from "@sveltejs/adapter-static";
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -8,6 +10,10 @@ const config = {
       pages: 'docs',
       assets: 'docs'
     }),
+
+    paths: {
+			base: dev ? '' : '/colorizer',
+		},
 
     prerender: {
       concurrency: 10,
